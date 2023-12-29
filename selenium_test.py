3f054d71a5d65fc98f58338ebd311c017104e426
Debian <admin@ip-172-31-43-219>
2023-12-29 17:36:00 +0100

    autorun.sh aws --produccion

--- selenium_test.py
+++ selenium_test.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+firefox_options = Options()
+firefox_capabilities = firefox_options.to_capabilities()
+
+# Headless mode Firefox
+firefox_options.add_argument("--headless")
+firefox_options.add_argument("--no-sandbox")
+firefox_options.add_argument("--disable-dev-shm-usage")
 
 try:
-    driver = webdriver.Firefox()
+    driver = webdriver.Firefox(options=firefox_options)
     driver.get("https://www.google.com")
     print(driver.title)
 finally:
     driver.quit()
+
