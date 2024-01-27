e09a548ec53344cbb6cff537058750aef5402f6d
davidcastagnetoa <davidcastagnetoa@gmail.com>
2024-01-27 21:26:02 +0100

    get cookies file in test folder

--- test/print_environItems.py
+++ test/print_environItems.py
@@ -1,6 +1,5 @@
-# Imprimir todas las variables de entorno
+# Imprimir todas las variables de entorno de la maquina actual
 import os
-
 
 for key, value in os.environ.items():
     print(f"{key}: {value}")
