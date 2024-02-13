b86ae2c58a7765eae626f63fb5b44954f8c8a948
David Castagneto <davidcastagnetoa@egmail.com>
2024-02-13 17:31:04 +0100

    feat(win and linux firewall custom message)

--- sendTelegramMessage.py
+++ sendTelegramMessage.py
@@ -1,7 +1,6 @@
 import requests
 from dotenv import load_dotenv
 import os
-import errno
 from utils.logger import log, API
 from halo import Halo
 
@@ -12,7 +11,7 @@ CHAT_ID = os.getenv("CHAT_ID")
 
 
 def send_message(token, chat_id, message):
-    spinner = Halo(text=f'Preparando envío de mensaje', spinner='dots')
+    spinner = Halo(text='Preparando envío de mensaje', spinner='dots')
     spinner.start()
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     datos = {
@@ -25,33 +24,25 @@ def send_message(token, chat_id, message):
         response = requests.post(url, data=datos)
         response.raise_for_status()
         API.write_log(f'Estado de la respuesta: {response.status_code}')
-        API.write_log(f"Mensaje enviado!")
+        API.write_log("Mensaje enviado!")
         spinner.succeed("Mensaje enviado!")
-        # log("📧")
         return response.json()
-    except requests.exceptions.ConnectionError as e:
-        # Capturar la excepcion de manera mas general y luego verificar el tipo de error
-        if e.args and isinstance(e.args[0], requests.packages.urllib3.exceptions.ProtocolError):
-            inner_exception = e.args[0].__context__
-            if isinstance(inner_exception, ConnectionResetError) and inner_exception.errno == errno.WSAECONNRESET:
-                API.write_log("Denegado, el firewall ha bloqueado esta petición. Consulte con el administrador de su red.")
-                spinner.fail("Denegado, el firewall ha bloqueado esta petición. Consulte con el administrador de su red.")
-            else:
-                API.write_log(f"Error de conexion: {e}")
-                spinner.fail(f"Error de conexión: {e}")
+    except requests.ConnectionError as e:
+        # Directamente capturamos ConnectionResetError desde la excepción de ConnectionError
+        if 'Connection aborted.' in str(e) and (
+                'Connection reset by peer' in str(e) or 'Se ha forzado la interrupción de una conexión existente por el host remoto' in str(e)):
+            custom_message = "Conexion interrumpida: parece que una politica de seguridad ha forzado la interrupcion de esta conexion. Esto puede deberse a restricciones impuestas por un firewall o configuraciones de seguridad en el servidor o la red local. Consulte con el administrador de su red para obtener mas detalles y posibles soluciones."
+            API.write_log(custom_message)
+            spinner.fail("Fallo de envio de mensaje, denegado por Firewall ")
         else:
-            API.write_log(f"Error de conexion: {e}")
-            spinner.fail(f"Error de conexión: {e}")
-        return None
-    except requests.HTTPError as e:
-        API.write_log(f"Error HTTP: {e}, Detalles: {response.text}")
-        spinner.fail("Fallo al enviar mensaje")
+            API.write_log(f"Error de conexión no especifico: {e}")
+            spinner.fail(f"Error de conexión no especifico: {e}")
         return None
     except Exception as e:
-        API.write_log(f"Error HTTP: {e}, Detalles: {response.text}")
+        API.write_log(f"Error desconocido: {e}")
         spinner.fail("Fallo al enviar mensaje")
         return None
 
 
-# # Ejemplo
+# Ejemplo de uso
 send_message(TOKEN, CHAT_ID, "Hola, soy un bot de Telegram")
